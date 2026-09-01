ad9b9759a58e87034485f51a926274b4256946fa
Boris Nzonteu <nzonteub@gmail.com>
2026-09-01 22:57:25 +0100

    Update datasource configuration for environment variables

---
+++ agriconnect/src/main/java/com/example/agriconnect/classes/PartageDiscussion.java
@@ -0,0 +1,4 @@
+package com.example.agriconnect.classes;
+
+public class PartageDiscussion {
+}
---
+++ agriconnect/src/main/java/com/example/agriconnect/repository/PartageDiscussionRepository.java
@@ -0,0 +1,4 @@
+package com.example.agriconnect.repository;
+
+public interface PartageDiscussionRepository {
+}
